2cdf15939fd6fd5011035c2285f18bea0f8d3351
Damizen2 <105399365+Damizen2@users.noreply.github.com>
2022-05-23 10:36:52 -0400

    Create DeepDishPizza.java

---
+++ DeepDishPizza.java
@@ -0,0 +1,8 @@
+public class DeepDishPizza extends Pizza{
+  double basePrice(){
+    return 10.99;
+  }
+  public String toString(){
+    return " Deep dish pizza priced at 10.99 dollars ";
+  }
+}
