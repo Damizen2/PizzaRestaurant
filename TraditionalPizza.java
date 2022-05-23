7707fdbe24a50a1bcb8b91d2e1355d66c160d7ab
Damizen2 <105399365+Damizen2@users.noreply.github.com>
2022-05-23 10:41:09 -0400

    Create TraditionalPizza.java

---
+++ TraditionalPizza.java
@@ -0,0 +1,8 @@
+public class TraditionalPizza extends Pizza{
+  double basePrice(){
+    return 8.99;
+  }
+  public String toString(){
+    return " Traditional Pizza priced at 8.99 dollars ";
+  }
+}
